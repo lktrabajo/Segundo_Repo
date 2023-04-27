5baa521be4e7908fe5995f03afdbb34b87798f69
lktrabajo <lktrabajo22@gmail.com>
2023-04-27 15:15:37 -0500

    Primera Version

---
+++ src/javaapplication9/JavaApplication9.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication9;
+
+/**
+ *
+ * @author LKmos
+ */
+public class JavaApplication9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+                
+    }
+    
+}
